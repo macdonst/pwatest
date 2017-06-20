07f06c53838802b3049e5b1bebffea15194e9b33
Simon MacDonald <simon.macdonald@gmail.com>
2017-06-20 10:22:29 -0700

    Add card in content section

--- sw.js
+++ sw.js
@@ -19,43 +19,43 @@ importScripts('workbox-sw.prod.v1.0.0.js');
  */
 const fileManifest = [
   {
-    "url": "/pwatest//css/index.css",
+    "url": "/pwatest/css/index.css",
     "revision": "e46d9a1c456a9c913ca10f3c16d50000"
   },
   {
-    "url": "/pwatest//index.html",
-    "revision": "b3ae9ba717e64a189c0af67fceece7f0"
+    "url": "/pwatest/index.html",
+    "revision": "14de139fd943cf04c7d15917f2c8568a"
   },
   {
-    "url": "/pwatest//js/index.js",
+    "url": "/pwatest/js/index.js",
     "revision": "b144c071205225b243caacc5b550f592"
   },
   {
-    "url": "/pwatest//static/js/app.b9724de07fbd3e7e845b.js",
-    "revision": "bf398f0bfbe3502fb28f2559cbe85c78"
+    "url": "/pwatest/static/js/app.f85802db0d92fa62e00c.js",
+    "revision": "d4cd8f540efb667a11909dc8c3385689"
   },
   {
-    "url": "/pwatest//static/js/manifest.36dcf09bf7e2f55ebd8a.js",
-    "revision": "a7ea42aab575b15d3b4786e513b8d055"
+    "url": "/pwatest/static/js/manifest.dd9d54cc83e1f5bdac0f.js",
+    "revision": "8146ae203d4739cddec54d98ab03f20b"
   },
   {
-    "url": "/pwatest//static/js/vendor.482c14416185ffb0ddad.js",
+    "url": "/pwatest/static/js/vendor.482c14416185ffb0ddad.js",
     "revision": "8b8eab7526c26db9e7b483dc88d1981b"
   },
   {
-    "url": "/pwatest//static/sw.js",
+    "url": "/pwatest/static/sw.js",
     "revision": "e9a53bbb3db1c2956bc5c0a16f819858"
   },
   {
-    "url": "/pwatest//static/workbox-sw.prod.v1.0.0.js",
+    "url": "/pwatest/static/workbox-sw.prod.v1.0.0.js",
     "revision": "9029a00430d1c6ccf363f3ad77c45d42"
   },
   {
-    "url": "/pwatest//sw.js",
-    "revision": "6f38020da0f5d40464f47801a6c7b2f0"
+    "url": "/pwatest/sw.js",
+    "revision": "cb837d103b331c97338da2ecb14420ed"
   },
   {
-    "url": "/pwatest//workbox-sw.prod.v1.0.0.js",
+    "url": "/pwatest/workbox-sw.prod.v1.0.0.js",
     "revision": "9029a00430d1c6ccf363f3ad77c45d42"
   }
 ];
